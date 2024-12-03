18d54d13ad6941b625fb180c0b7418a480658176
user <user@mail.com>
2024-12-03 12:40:56 +0100

    day_1 part 2

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,4 +1,8 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use std::fs::read_to_string;
+use std::collections::HashMap;
 
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
@@ -6,7 +10,6 @@ fn read_lines(filename: &str) -> Vec<String> {
     for line in read_to_string(filename).unwrap().lines() {
         result.push(line.to_string())
     }
-
     result
 }
 
@@ -23,7 +26,7 @@ fn part_1(){
         right.push(right_val);
     }
 
-    
+
     left.sort();
     right.sort();
     let mut sum = 0;
@@ -34,6 +37,41 @@ fn part_1(){
     println!("{}", sum);
 }
 
+
+fn part_2(){
+    let input = "input.txt";
+    let lines = read_lines(&input);
+    let mut left = HashMap::<i32, i32>::new();
+    let mut right = HashMap::<i32, i32>::new();
+    for line in lines.iter(){
+        let mut line_split = line.split("   ");
+        let left_val = line_split.next().unwrap().parse::<i32>().unwrap();
+        let right_val = line_split.next().unwrap().parse::<i32>().unwrap();
+
+        match left.get(&left_val){
+            Some(val) => left.insert(left_val, val + 1),
+            None => left.insert(left_val, 1),
+        };
+
+        match right.get(&right_val){
+            Some(val) => right.insert(right_val, val + 1),
+            None => right.insert(right_val, 1),
+        };
+        
+    }
+
+    let mut sum = 0;
+    for (key, val) in left.iter() {
+        match right.get(key){
+            Some(v) => sum += key * val * v,
+            _ => (),
+        };
+    }
+
+    println!("{}", sum);
+}
+
 fn main(){
-    part_1();
+    //part_1();
+    part_2();
 }
