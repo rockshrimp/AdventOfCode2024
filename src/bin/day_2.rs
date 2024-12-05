dfcb66b315f46f1b093490dd2e49da50f633aeba
Lucas <lucasplissonnier@gmail.com>
2024-12-05 11:20:22 +0100

    day_2 part 1

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -2,7 +2,6 @@
 #![allow(unused_variables)]
 
 use std::fs::read_to_string;
-use std::collections::HashMap;
 
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
@@ -13,65 +12,41 @@ fn read_lines(filename: &str) -> Vec<String> {
     result
 }
 
+fn is_safe(values: &Vec<i32>) -> bool{
+    //ascending
+    if values.windows(2).all(|w| w[0] < w[1] && (w[1] - w[0]) <= 3 && (w[1] - w[0]) >= 1){
+        return true;
+    }
+    //descending
+    if values.windows(2).all(|w| w[0] > w[1] && (w[0] - w[1]) <= 3 && (w[0] - w[1]) >= 1){
+        return true;
+    }
+    false 
+}
+
 fn part_1(){
     let input = "input.txt";
     let lines = read_lines(&input);
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-    for line in lines.iter(){
-        let mut line_split = line.split("   ");
-        let left_val = line_split.next().unwrap().parse::<i32>().unwrap();
-        let right_val = line_split.next().unwrap().parse::<i32>().unwrap();
-        left.push(left_val);
-        right.push(right_val);
+
+    let mut valid_lines_count = 0;
+    for (idx, line) in lines.iter().enumerate(){
+        let line_split = line.split(" ");
+        let values: Vec<i32> = line_split.map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();        
+        
+        if is_safe(&values){
+            valid_lines_count += 1;
+        }
     }
 
-
-    left.sort();
-    right.sort();
-    let mut sum = 0;
-    for i in 0..left.len(){
-        sum += (right[i] - left[i]).abs()
-    }
-
-    println!("{}", sum);
+    println!("{}", valid_lines_count);
 }
-
 
 fn part_2(){
     let input = "input.txt";
     let lines = read_lines(&input);
-    let mut left = HashMap::<i32, i32>::new();
-    let mut right = HashMap::<i32, i32>::new();
-    for line in lines.iter(){
-        let mut line_split = line.split("   ");
-        let left_val = line_split.next().unwrap().parse::<i32>().unwrap();
-        let right_val = line_split.next().unwrap().parse::<i32>().unwrap();
-
-        match left.get(&left_val){
-            Some(val) => left.insert(left_val, val + 1),
-            None => left.insert(left_val, 1),
-        };
-
-        match right.get(&right_val){
-            Some(val) => right.insert(right_val, val + 1),
-            None => right.insert(right_val, 1),
-        };
-        
-    }
-
-    let mut sum = 0;
-    for (key, val) in left.iter() {
-        match right.get(key){
-            Some(v) => sum += key * val * v,
-            _ => (),
-        };
-    }
-
-    println!("{}", sum);
 }
 
 fn main(){
-    //part_1();
-    part_2();
+    part_1();
+    //part_2();
 }
