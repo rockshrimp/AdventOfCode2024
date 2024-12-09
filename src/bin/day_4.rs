9226b51aef6cda7e42bfe9ed524e864e24fc6b3b
Lucas <lucasplissonnier@gmail.com>
2024-12-10 00:58:30 +0100

    dy 4 part 2

--- src/bin/day_4.rs
+++ src/bin/day_4.rs
@@ -1,6 +1,7 @@
 use advent_of_code_2024::read_lines;
 
 fn east(x:usize, y:usize, grid: &Vec<String>, word: &str)-> bool{
+    //println!("{} {} {} found east match", x, y, &grid[x][y - word.len()..y]);
     return grid[x][y..y + word.len()] == *word;
 }
 
@@ -83,8 +84,6 @@ fn south_west(x:usize, y:usize, grid: &Vec<String>, word: &str)-> bool{
     return diag == word;
 }
 
-
-
 fn detect_word(x:usize, y:usize, grid: &Vec<String>) -> i32 {
     let word = "XMAS";
     let width = grid[0].len() as i32;
@@ -105,53 +104,45 @@ fn detect_word(x:usize, y:usize, grid: &Vec<String>) -> i32 {
 
     if valid_west{
         if west(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
 
     if valid_south{
         if south(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
 
     if valid_north{
         if north(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
 
     if valid_north && valid_east{
         if north_east(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
     
     if valid_south && valid_east{
         if south_east(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
 
     if valid_north && valid_west{
         if north_west(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
 
     if valid_south && valid_west{
         if south_west(x, y, grid, word){
-            //println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
             count += 1;
         }
     }
-
     return count;
 }
 
@@ -170,91 +161,20 @@ fn part_1(){
     println!("result : {}", result);
 }
 
-fn detect_word_2(x:usize, y:usize, grid: &Vec<String>) -> i32 {
+fn detect_word_2(x:usize, y:usize, grid: &Vec<String>) -> bool {
     let word = "MAS";
     let width = grid[0].len();
     let height = grid.len();
-    let mut count = 0;
 
-    if x > 0 && x + 1 < height && y > 0 && y + 1 < width{
-        //if east{
-        if east(x, y, grid, word){
-            count += 1;
+    if x > 0 && x + 1 < height && y > 0 && y + 1 < width{ 
+        if (north_east(x + 1, y - 1, grid, word) || south_west(x - 1, y + 1, grid, word)) &&
+        (south_east(x - 1, y - 1, grid, word) || north_west(x + 1, y + 1, grid, word)) 
+        {
+            return true;
         }
-
-        
-        //if west{
-        if grid[x][y - 1 ..y + 2] == *word.chars().rev().collect::<String>(){
-            println!("{} {} {} found west match", x, y, &grid[x][y - word.len()..y]);
-            count += 1;
-        }
-        
-        /*
-        let mut column = String::new();
-        for i in x..x+word.len(){
-            column.push(grid[i].chars().nth(y).unwrap());
-        } 
-        if column == word{
-            //println!("{} {} {} found south match", x, y, column);
-
-            count += 1;
-        }
-    
-    
-        let mut column = String::new();
-        for i in 0..word.len(){
-            column.push(grid[x - i].chars().nth(y).unwrap());
-        }
-
-        if column == word{
-            //println!("{} {} {} found north match", x, y, column);
-            count += 1;
-        }
-    
-    
-        let mut diag = String::new(); 
-        for i in 0..word.len(){
-            diag.push(grid[x - i].chars().nth(y + i).unwrap());
-        }
-
-        if diag == word{
-            //println!("{} {} {} found north-east match", x, y, diag);
-            count += 1;
-        }
-        
-        let mut diag = String::new(); 
-        for i in 0..word.len(){
-            diag.push(grid[x + i].chars().nth(y + i).unwrap());
-        }
-
-        if diag == word{
-            //println!("{} {} {} found south-east match", x, y, diag);
-            count += 1;
-        }
-    
-        let mut diag = String::new(); 
-        for i in 0..word.len(){
-            diag.push(grid[x - i].chars().nth(y - i).unwrap());
-        }
-
-        if diag == word{
-            //println!("{} {} {} found north-west match", x, y, diag);
-            count += 1;
-        }
-
-        let mut diag = String::new(); 
-        for i in 0..word.len(){
-            diag.push(grid[x + i].chars().nth(y - i).unwrap());
-        }
-
-        if diag == word{
-            //println!("{} {} {} found south-west match", x, y, diag);
-            count += 1;
-        }
-        */
     }
 
-    return 0;
+    return false;
 }
 
 fn part_2(){
@@ -265,8 +185,9 @@ fn part_2(){
     for x in 0..lines.len(){
         for (y, ch) in lines[x].chars().enumerate(){
             if ch == 'A'{
-                println!("Found A at {} {}", x, y);
-                result += detect_word_2(x, y, &lines);
+                if detect_word_2(x, y, &lines){
+                    result += 1;
+                }
             }
         }
     }
@@ -274,6 +195,6 @@ fn part_2(){
 }
 
 fn main(){
-    part_1();
-    //part_2();
+    //part_1();
+    part_2();
 }
