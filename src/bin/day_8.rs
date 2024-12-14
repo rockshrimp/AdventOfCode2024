d2facc42d898aa20cb83ca2159c6f259ddfd8de5
Lucas <lucasplissonnier@gmail.com>
2024-12-14 11:21:06 +0100

    day_8 part 2

--- src/bin/day_8.rs
+++ src/bin/day_8.rs
@@ -37,7 +37,6 @@ fn part_1(){
                 .or_insert_with(Vec::new) 
                 .push((x as i32, y as i32));
             }
-            
         }
     }
 
@@ -54,13 +53,87 @@ fn part_1(){
             //println!("{:?}", get_antinode_positions(*perm[0], *perm[1]));
         }
     }
-
-    
     println!("{:?}", antinodes.len());
     
 }   
 
+fn get_antinode_positions_2(a:Position, b: Position, width: i32, height: i32) -> HashSet<Position>{
+    let mut antinodes = HashSet::new();
+    //println!("{:?} & {:?}", a, b);
+
+    if get_norm(a) > get_norm(b){
+        let diff = substract_coordinates(a, b);
+        let mut antinode_pos = a;
+
+        while coordinates_in_bounds(width, height, antinode_pos){
+            antinodes.insert(antinode_pos);
+            //println!("{:?} + {:?}", antinode_pos, diff);
+            antinode_pos = add_coordinates(antinode_pos, diff);
+        }
+        let mut antinode_pos = b;
+        while coordinates_in_bounds(width, height, antinode_pos){
+            antinodes.insert(antinode_pos);
+            //println!("{:?} - {:?}", antinode_pos, diff);
+            antinode_pos = substract_coordinates(antinode_pos, diff);
+        }
+
+        return antinodes;
+        //return (add_coordinates(a, diff), (substract_coordinates(b, diff)));
+    }else{
+        let diff = substract_coordinates(b, a);
+        //println!("{:?}, {:?}, {:?}", diff, add_coordinates(a, diff), substract_coordinates(b, diff));
+        let mut antinode_pos = b;
+        
+        while coordinates_in_bounds(width, height, antinode_pos){
+            antinodes.insert(antinode_pos);
+            antinode_pos = add_coordinates(antinode_pos, diff);
+            //println!("adding {:?} + {:?} = {:?}", antinode_pos, diff, add_coordinates(b, diff));
+        }
+        let mut antinode_pos: (i32, i32) = a;
+        while coordinates_in_bounds(width, height, antinode_pos){
+            antinodes.insert(antinode_pos);
+            antinode_pos = substract_coordinates(antinode_pos, diff);
+            //println!("{:?} - {:?}", antinode_pos, diff);
+        }
+        return antinodes;
+    }
+}
+
+fn part_2(){
+    let input = "input.txt";
+    let lines: Vec<String> = read_lines(&input);
+
+    let height = lines.len() as i32;
+    let width = lines[0].len() as i32;
+
+    let mut char_map = HashMap::new();
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+
+    for (x, lines) in lines.iter().enumerate(){
+        for (y, c) in lines.chars().enumerate(){
+            if c.is_alphanumeric(){
+                char_map.entry(c)
+                .or_insert_with(Vec::new) 
+                .push((x as i32, y as i32));
+            }
+        }
+    }
+
+    for (_, pos_vec) in &char_map{
+        let perms = pos_vec.iter().permutations(2).collect::<Vec<_>>();
+        for perm in perms{
+            let new_antinodes = get_antinode_positions_2(*perm[0], *perm[1], width, height);
+            antinodes.extend(new_antinodes);
+            //println!("{:?}", get_antinode_positions(*perm[0], *perm[1]));
+        }
+    }
+    //println!("{:?}", antinodes);
+
+    println!("{:?}", antinodes.len());
+    
+}
+
 fn main(){
-    part_1();
-    //part_2();
+    //part_1();
+    part_2();
 }
