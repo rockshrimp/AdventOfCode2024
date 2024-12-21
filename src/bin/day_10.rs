bc13ca57725a2108905441720a6d7737dd095cb7
Lucas <lucasplissonnier@gmail.com>
2024-12-21 13:42:59 +0100

    day_10 part 2

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -52,10 +52,61 @@ fn part_1(){
     }
 
     println!("{}", scores_sum);
-
-
 }
 
+fn get_trailhead_score_2(pos:Position, grid: &Vec<Vec<i32>>, width: usize, height: usize) -> i32{
+    let mut queue: Vec<(i32, i32)> = vec![pos];
+    let mut score = 0;
+
+    while queue.len() > 0{
+        let pos = queue.pop().unwrap();
+        let val = grid[pos.0 as usize][pos.1 as usize];
+        if val == 9{
+            score += 1;
+        }
+
+        //println!("[{}, {}] val :{}", pos.0, pos.1, val);
+        for neighbour in get_neighbours(pos){
+            if coordinates_in_bounds(width as i32, height as i32, neighbour){
+                if grid[neighbour.0 as usize][neighbour.1 as usize] == val + 1{
+                    queue.push(neighbour);
+                }
+            }
+        }
+    }
+    return score;
+}
+
+
+fn part_2(){
+    let input = "input.txt";
+    let lines: Vec<String> = read_lines(&input);
+    
+    let mut grid: Vec<Vec<i32>> = Vec::new();
+    for line in lines{
+        grid.push(line.chars().map(|c| if c!= '.' {c.to_digit(10).unwrap() as i32} else {11}).collect());
+    }
+
+    let height = grid.len();
+    let width = grid[0].len();
+
+    let height = grid.len();
+    let width = grid[0].len();
+
+    let mut scores_sum = 0;
+    for (x, line) in grid.iter().enumerate(){
+        //println!("{:?}", line);
+        for (y, val) in line.iter().enumerate(){
+            if *val == 0{
+                //println!("0 found at {},{}", x, y);
+                scores_sum += get_trailhead_score_2((x as i32, y as i32), &grid, width, height);
+            }
+        } 
+    }
+
+    println!("{}", scores_sum);
+}
 fn main(){
-    part_1();
+    //part_1();
+    part_2();
 }
